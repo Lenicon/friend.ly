4238a6c946290c474e1250565ced23f75b204b28
Lenicon <lenthesmiledev@gmail.com>
2024-02-25 04:58:44 +0800

    lol

--- src/pages/Messenger.tsx
+++ src/pages/Messenger.tsx
@@ -3,7 +3,6 @@ import Content from "../components/Content";
 import Sidebar from "../components/Sidebar";
 
 export default function Messenger() {
-  // const [chat, setChat] = useState(false);
   return (
     <div className="messenger">
       <Sidebar/>
